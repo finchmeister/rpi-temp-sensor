a5949d281bc56d646159f14cb40b43d622933561
Jonathan Finch <thefinchmeister@gmail.com>
2020-12-28 21:21:39 +0000

    Fix restore db

--- recover-git-data/restore_db.py
+++ recover-git-data/restore_db.py
@@ -8,15 +8,30 @@ port = 8086
 user = "rpi"
 password = "rpi"
 db_name = "sensor_data"
-tempdata_path = "/Users/jofinch/PersonalProjects/bedroom-temperature-api"
+tempdata_path = "/home/pi/bedroom-temperature-api"
+# tempdata_path = "/Users/jfinch/PersonalProjects/bedroom-temperature-api"
 client = InfluxDBClient(host, port, user, password, db_name)
+checkpoint_path = os.path.dirname(os.path.abspath(__file__)) + "/checkpoint.txt"
+
+
+def reached_checkpoint(commit_hash):
+    return commit_hash == get_checkpoint()
+
+
+def save_checkpoint(commit_hash):
+    f = open(checkpoint_path, "w")
+    f.write(commit_hash)
+    f.close()
+
+
+def get_checkpoint():
+    f = open(checkpoint_path, "r")
+    return f.read().strip()
 
 
 def get_data(commit_hash):
     stream = os.popen("cd " + tempdata_path + " && git show " + commit_hash + ":temperature.json | cat")
-    data = [json.loads(stream.read())]
-    print(data)
-    write_data(data)
+    return [json.loads(stream.read())]
 
 
 def write_data(data):
@@ -24,16 +39,33 @@ def write_data(data):
         return
     client.write_points(data)
 
-# Loop through every commit
 
-# stream = os.popen("cd " + tempdata_path + " && git rev-list --reverse master")
-stream = os.popen("cd " + tempdata_path + " && git rev-list master")
-output = stream.readlines()
+def pull_latest():
+    os.popen("cd " + tempdata_path + " && git pull")
 
-i = 0
 
-for line in output:
-    commit_hash = line.strip()
-    print(i)
-    i = i+1
-    print(get_data(commit_hash))
+def get_commits_in_reverse_chronological():
+    stream = os.popen("cd " + tempdata_path + " && git rev-list master")
+    return stream.readlines()
+
+
+def run():
+    pull_latest()
+
+    next_checkpoint = None
+    for line in get_commits_in_reverse_chronological():
+        commit_hash = line.strip()
+
+        if next_checkpoint is None:
+            next_checkpoint = commit_hash
+
+        if reached_checkpoint(commit_hash) is True:
+            break
+
+        data = get_data(commit_hash)
+        write_data(data)
+
+    save_checkpoint(next_checkpoint)
+
+
+run()
